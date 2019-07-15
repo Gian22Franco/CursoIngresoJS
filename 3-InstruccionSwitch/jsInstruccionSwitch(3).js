02a1eb967bb7329ddc174191c68acad57ffb391d
Gian22Franco <gian.lofeudo94@gmail.com>
2019-07-15 11:34:39 -0300

    GIT

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -3,8 +3,31 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
+switch (mesDelAño)
+{
+
+    case"Enero":
+    case "Marzo" :
+    case "Abril":
+    case "Mayo":
+    case "Junio":
+    case "Julio":
+    case "Agosto":
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre":
+    case"Diciembre":
+        alert ("Este mes tiene 30 dìas");
+    break;
+
+    case"Febrero":
+        alert ("Este mes tiene menos de 29 dìas" );
+    break;
+    // default: 
+            //alert ("Este mes tiene 30 dias" );
+    // ** Otra forma de hacerlo**
+}
+
 	
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -3,10 +3,30 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+switch (mesDelAño) {
+
+case"Enero":
+case "Marzo" :
+case "Mayo":
+case "Julio":
+case "Agosto":
+case "Octubre":
+case"Diciembre":
+    alert("Este mes tiene 31 dìas");
+    
+break;
 	
-	
 
+case "Abril":
+case  "Junio":
+case "Noviembre":
+case "Septiembre":
+        alert("Este mes tiene 30 dìas");
+break;
 
+default:
+    alert("Este mes tiene 29 dìas");
+    break;
 
+}
 }//FIN DE LA FUNCIÓN
